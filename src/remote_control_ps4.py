1cc9f4a2abaf22aae148d923fee8bd970c167ee0
Miguel <piprojectsms@gmail.com>
2020-06-03 01:17:47 +0200

    Add head support

--- src/remote_control_ps4.py
+++ src/remote_control_ps4.py
@@ -7,6 +7,7 @@ from geometry_msgs.msg import Twist
 from andrewbot_utils.math_utils import translate_point_to_twist
 
 
+
 # LEFT JOYSTICK
 BASE_HORIZONTAL = 0 
 BASE_VERTICAL = 1
@@ -24,8 +25,23 @@ def move_base(joy_data):
 
     base_movement.linear.x, base_movement.angular.z = translate_point_to_twist(x, y)
 
+
+
+# RIGHT JOYSTICK
+HEAD_HORIZONTAL = 3
+HEAD_VERTICAL = 4
+
+head_movement = Twist()
+head_motion_pub = rospy.Publisher("andrewbot/HeadCommand", Twist, queue_size=1)
+
+def move_head(joy_data):
+    head_movement.angular.x = -joy_data.axes[HEAD_HORIZONTAL]
+    head_movement.angular.z =  joy_data.axes[HEAD_VERTICAL]
+
+
 def joy_data_callback(joy_data):
     move_base(joy_data)
+    move_head(joy_data)
     
     
 
@@ -40,6 +56,7 @@ if __name__ == '__main__':
         rate = rospy.Rate(100)  # 10ms    100
         while not rospy.is_shutdown():
             base_motion_pub.publish(base_movement)
+            head_motion_pub.publish(head_movement)
             rate.sleep()
             
     except rospy.ROSInterruptException:
